d996ddf63d0c41af67b4f6c8f5f1b92cff91d41e
root <arko.andrew2000@gmail.com>
2020-11-29 21:17:04 +0000

    add start script and port

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+const port = process.env.PORT || 3000
+app.listen(port, function(){
 	console.log("YelpCamp has started!!!");
 });
